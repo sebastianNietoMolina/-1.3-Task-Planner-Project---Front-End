9f7e9f35d9cd53311f255f0d1a5a5221a36ce77c
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2021-02-23 23:43:24 -0500

    heroku 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Login} from './componentes/Login'
+import {DrawerLogin} from './componentes/Drawer'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Holi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const user = {
+  mail: localStorage.setItem('mail', "juan@gmail.com"),
+  password: localStorage.setItem('pw', "prueba")
+};
+
+const myData = {
+  mail : localStorage.getItem('mail'),
+  password : localStorage.getItem("pw")
 }
+
+const LoginView = () => (
+  <Login password={myData.password}  mail={myData.mail}/>
+);
+
+const DrawerView = () => (
+  <DrawerLogin/>
+);
+
+class App extends React.Component {
+ 
+  constructor(props){
+    super(props)
+    this.state = {isLoogedIn : false}
+    this.handleIslooged = this.handleIslooged.bind(this)
+}
+
+  render(){
+    return (
+        <Router>
+          <div onSubmit={this.handleIslooged}>          
+            <div>
+              { this.state.isLoogedIn ?
+                <Route component={DrawerView} /> :
+                <Route path="/" component={LoginView}/>
+              }
+            </div>
+          </div>
+        </Router>
+      );
+    }
+
+    handleIslooged (e) {
+
+      e.preventDefault();
+
+      if( localStorage.getItem("isLoogedIn") === "true" ){
+          this.setState({
+              isLoogedIn : true
+          });
+      }else{
+          this.setState({
+              isLoogedIn : false
+          });
+      } 
+  }
+}
+
 
 export default App;
